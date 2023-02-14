6e9b59c5f39a63eb962e3940ac53923437ad81cc
Pereyra Jose <pereyrajose848@gmail.com>
2023-02-13 21:18:13 -0300

    agregue footer con un poco de Style

--- src/components/commons/Footer.jsx
+++ src/components/commons/Footer.jsx
@@ -1,8 +1,13 @@
+import { Container } from "react-bootstrap";
 
 
 const Footer = () => {
     return (
-       <h2>Este es el Footer</h2>
+        <section className="bg-dark text-light">
+       <Container  className="text-center p-2">
+<h2>Este es en footer</h2>
+       </Container>
+       </section>
     );
 };
 
