804bb3263bbd52da3efdddd4083a88935f321423
Pereyra Jose <pereyrajose848@gmail.com>
2023-02-16 20:16:08 -0300

    configuramos los navlink asi como dimos un poco de style a components commons

--- src/components/commons/Footer.jsx
+++ src/components/commons/Footer.jsx
@@ -1,14 +1,20 @@
 import { Container } from "react-bootstrap";
 
-
 const Footer = () => {
-    return (
-        <section className="bg-dark text-light">
-       <Container  className="text-center p-2">
-<h2>Este es en footer</h2>
-       </Container>
-       </section>
-    );
+  return (
+    <section className="bg-dark text-light">
+      <Container className="text-center p-2">
+        <section className="d-flex justify-content-evenly my-4 text-white-50">
+          <div>
+            <p>&copy; Todos los derechos reservados</p>
+          </div>
+          <div>
+            <h5>CALISTENIA EVOLUTIVA <br />TUCUMAN</h5>
+          </div>
+        </section>
+      </Container>
+    </section>
+  );
 };
 
 export default Footer;
--- src/components/commons/Menu.jsx
+++ src/components/commons/Menu.jsx
@@ -1,31 +1,40 @@
-import { Col } from 'react-bootstrap';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import { Col } from "react-bootstrap";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { Link, NavLink } from "react-router-dom";
+import icono from "../img/icono.jpeg";
 
 
 const Menu = () => {
-    return (
-        <section className='bg-dark'>
-      <Container  className='p-3'>
-        <Navbar bg="dark" expand="lg" variant="dark" className="row justify-content-between m-0 p-0">
-        <Col lg={5} className="text-lg-start text-center">
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        </Col>
-        <Col lg={5}>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-         
-          </Nav>
-        </Navbar.Collapse>
-        </Col>
-    </Navbar>
+  return (
+    <section className="bg-black">
+      <Container className="p-3">
+        <Navbar
+          bg="black"
+          expand="lg"
+          variant="dark"
+          className="row justify-content-between m-0 p-0"
+        >
+          <Col lg={5} className="text-lg-start text-center">
+            <Navbar.Brand as={Link} to="/">
+              <img src={icono} alt="icono" className="w-25 m-0 p-0" />
+            </Navbar.Brand>
+          </Col>
+          <Col lg={5}>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+               <NavLink className="btn text-light" to="*">Pagos</NavLink>
+               <NavLink className="btn text-light" to="/administrar">Administrar</NavLink>
+               <NavLink className="btn text-light" to="*">Login</NavLink>
+              </Nav>
+            </Navbar.Collapse>
+          </Col>
+        </Navbar>
       </Container>
-      </section>
-    );
+    </section>
+  );
 };
 
 export default Menu;
